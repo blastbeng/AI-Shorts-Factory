28aef9c66bcca5bf632b718bc736c501a75e9cb5
blastbeng <rodfval.fv@gmail.com>
2026-07-27 23:30:41 +0200

    qaaa

--- scripts/download_llama_cpp.sh
+++ scripts/download_llama_cpp.sh
@@ -7,8 +7,8 @@ echo "=== Download del modello LLM (Qwen GGUF) ==="
 sudo mkdir -p /opt/models
 
 # Scarica il modello Qwen
-MODEL_URL="https://huggingface.co/DavidAU/Llama-3.2-8X3B-MOE-Dark-Champion-Instruct-uncensored-abliterated-18.4B-GGUF/resolve/main/L3.2-8X3B-MOE-Dark-Champion-Inst-18.4B-uncen-ablit_D_AU-Q4_k_m.gguf"
-MODEL_PATH="/opt/models/L3.2-8X3B-MOE-Dark-Champion-Inst-18.4B-uncen-ablit_D_AU-Q4_k_m.gguf"
+MODEL_URL="https://huggingface.co/DavidAU/Llama-3.2-8X3B-MOE-Dark-Champion-Instruct-uncensored-abliterated-18.4B-GGUF/resolve/main/L3.2-8X3B-MOE-Dark-Champion-Inst-18.4B-uncen-ablit_D_AU-Q6_k.gguf"
+MODEL_PATH="/opt/models/L3.2-8X3B-MOE-Dark-Champion-Inst-18.4B-uncen-ablit_D_AU-Q6_k.gguf"
 
 # Verifica se il file esiste e ha una dimensione maggiore di 0
 if [ -f "$MODEL_PATH" ] && [ -s "$MODEL_PATH" ]; then
